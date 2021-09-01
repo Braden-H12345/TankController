e0f77a243ff98fd8927327fd04be70e956529fe1
BradenH <bradenhonea@yahoo.com>
2021-09-01 01:17:13 -0500

    Finished initial setup
    Finished setup. Now have player, pickups, and some enemies

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TankController))]
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] int _maxHealth = 3;
+    int _currentHealth;
+
+    TankController _tankController;
+    private void Awake()
+    {
+        _tankController = GetComponent<TankController>();
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        _currentHealth += amount;
+        Debug.Log("Player Health: " + _currentHealth);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Players health: " + _currentHealth);
+
+        if(_currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        //play partciles & sounds
+    }
+}
